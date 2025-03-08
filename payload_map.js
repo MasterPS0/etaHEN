e2bd2bd55cb6afdb4bea9684a8427c3b0f8b114f
Master <49209220+Master-s@users.noreply.github.com>
2025-03-08 23:46:35 +0400

    Add files via upload

--- payload_map.js
+++ payload_map.js
@@ -71,6 +71,18 @@ const payload_map = [
         projectSource: "https://github.com/LightningMods/etaHEN",
         binarySource: "https://github.com/LightningMods/etaHEN/releases/download/1.9b/etaHEN.bin",
         version: "2.0b",
+		supportedFirmwares: ["5.10"],
+		toPort: 9021
+    },
+		{
+        displayTitle: "etaHEN20b", 
+        description: "AIO HEN 5.50",
+        fileName: "etaHEN20b550.bin",
+        author: "LightningMods",
+        projectSource: "https://github.com/LightningMods/etaHEN",
+        binarySource: "https://github.com/LightningMods/etaHEN/releases/download/1.9b/etaHEN.bin",
+        version: "2.0b",
+		supportedFirmwares: ["5.50"],
 		toPort: 9021
     },
 	{
